e991f857b01d07d5693ee2db2181b606e4934e08
aliakbaridrus <aliidrus3142@gmail.com>
2026-07-08 17:58:49 +0700

    tambahan

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/foundit/index.php');
-});
-
-Route::get('/foundit', function () {
-    return redirect('/foundit/index.php');
+    return redirect('/Foundit');
 });
